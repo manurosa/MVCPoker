7add6b9c56c1ddd55ad1fa3180aaf2defa1c7a79
manurosa <manurosa11@gmail.com>
2016-04-19 09:57:47 +0200

    added input and output (incomplete)models

---
+++ WebApplication/WebApplication/Models/PokerInputModels.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication.Models
+{
+    public class PokerInputModels
+    {
+        [Required, Range(0, int.MaxValue, ErrorMessage = "Value must be greater than 0")]
+        [Display(Name = "Small Blind")]
+        public int SmallBlind { get; set; }
+        [Required, Range(0, int.MaxValue, ErrorMessage = "Value must be greater than 0")]
+        [Display(Name = "Big Blind")]
+        public int BigBlind { get; set; }
+        [Range(0, int.MaxValue, ErrorMessage = "Value must be greater than 0")]
+        public int Ante { get; set; }
+        [Required, Range(0, int.MaxValue, ErrorMessage = "Value must be greater than 0")]
+        public int Players { get; set; }
+        [Required, Range(0, int.MaxValue, ErrorMessage = "Value must be greater than 0")]
+        public int Stack { get; set; }
+        [Required, Range(0, int.MaxValue, ErrorMessage = "Value must be greater than 0")]
+        [Display(Name = "Remaining Opponents")]
+        public int RemainingOpp { get; set; }
+    }
+}
---
+++ WebApplication/WebApplication/Models/PokerOutputModels.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication.Models
+{
+    public class PokerOutputModels
+    {
+        [Required]
+        public int CPR { get; private set; }
+        [Required]
+        public int CSI { get; private set; }
+        [Required]
+        public int PN { get; private set; }
+
+    }
+}
